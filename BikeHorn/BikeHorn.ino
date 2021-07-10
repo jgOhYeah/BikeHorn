327d82630d786c1a21feb3392e42fd13b4165bc2
Jotham Gates <jgat0002@student.monash.edu>
2021-07-10 23:24:46 +1000

    Added switch in clamp & made intellisense happy

--- BikeHorn/BikeHorn.ino
+++ BikeHorn/BikeHorn.ino
@@ -23,7 +23,7 @@ void setup() {
     Serial.println(F(" tunes installed"));
 
     // Tune Player
-    flashLoader.setTune(pgm_read_word(&(tunes[curTune])));
+    flashLoader.setTune((uint16_t*)pgm_read_word(&(tunes[curTune])));
     tune.begin(&flashLoader, &piezo);
     tune.spool();
 
@@ -85,7 +85,7 @@ void loop() {
         }
         Serial.print(F("Changing to tune "));
         Serial.println(curTune);
-        flashLoader.setTune(pgm_read_word(&(tunes[curTune])));
+        flashLoader.setTune((uint16_t*)pgm_read_word(&(tunes[curTune])));
 
         // Only let go once button is not pushed for more than DEBOUNCE_TIME
         uint32_t startTime = millis();
@@ -160,4 +160,55 @@ void startBoost() {
     TCCR2A = (1 << COM2A1) | (1 << WGM21) | (1 << WGM20); // Mode 3, fast PWM, reset at 255
     TCCR2B = (1<< CS20); // Prescalar 1
     OCR2A = IDLE_DUTY; // 76.6% duty
+}
+
+/** Mode for a midi synth */
+void midiSynth() {
+    // Flashing lights to warn of being in this mode
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_EXTERNAL, HIGH);
+
+    startBoost();
+    uint8_t currentNote;
+
+    while(digitalRead(BUTTON_HORN)) {
+        uint8_t incomingNote; // The next byte to be read off the serial buffer.
+        uint8_t midiPitch; // The MIDI note number.
+        uint8_t midiVelocity; //The velocity of the note.
+
+        incomingNote = getByte();
+        // Analyse the byte. The byte at this stage should be a status byte. If it isn't, the byte is ignored.
+        switch (incomingNote) {
+            case B10010000 | MIDI_CHANNEL: // Note on
+            midiPitch = getByte();
+            if(midiPitch != -1) {
+                midiVelocity = getByte();
+                if(midiVelocity != -1) {
+                    piezo.playMidiNote(midiPitch); // This will handle the conversion into note and octave.
+                    digitalWrite(LED_BUILTIN, HIGH);
+                    digitalWrite(LED_EXTERNAL, LOW);
+                    currentNote = midiPitch;
+                }
+            }
+            break;
+
+            case B10000000 | MIDI_CHANNEL: // Note off
+            midiPitch = getByte();
+            if(midiPitch == currentNote) {
+                piezo.stopSound();
+                digitalWrite(LED_BUILTIN, LOW);
+                digitalWrite(LED_EXTERNAL, HIGH);
+            }
+            break;
+        }
+    }
+    piezo.stopSound();
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_EXTERNAL, LOW);
+}
+
+/** Wait for an incoming note and return the note. */
+byte getByte() {
+    while (!Serial.available() && digitalRead(BUTTON_HORN)) {} //Wait while there are no bytes to read in the buffer.
+    return Serial.read();
 }
