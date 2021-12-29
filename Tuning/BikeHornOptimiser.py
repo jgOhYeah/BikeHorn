464819931ae8fc983c6e1e2f7001fc820d856e75
Jotham Gates <jgat0002@student.monash.edu>
2021-12-29 14:59:47 +1100

    Added file menus and help tab

--- Tuning/BikeHornOptimiser.py
+++ Tuning/BikeHornOptimiser.py
@@ -2,8 +2,9 @@
 # GUI libraries
 import tkinter as tk                    
 from tkinter import ttk
+from tkinter import filedialog as fd
 import tkinter.messagebox as tkmb
-from typing import Iterable
+import webbrowser
 
 # Serial
 import serial.tools.list_ports
@@ -17,12 +18,39 @@ import sounddevice as sd
 import time
 import numpy as np
 import threading
+from typing import Iterable
+
+version = "0.0.1a"
+
+class FilesManager():
+    
+    def __init__(self, logging):
+        self._filename = "TODOMakeThisADate.npz"
+        self._logging = logging
+
+    def get_filename(self):
+        return self._filename
+
+    def set_filename(self, filename):
+        self._filename = filename
+    
+    def save_data(self):
+        self._logging.info("If it were implemented, I would be writing a file")
+    
+    def open(self, filename):
+        self._logging.info("If it were implemented, I would be opening a file")
 
 class ResultsManager():
     # For generating the functions:
     # 1. Smooth the data
     # 2. Numpy has a piecewise functions tool
     # Have the number of functions as an option in the gui?
+    def clear_data(self):
+        pass
+        # TODO
+
+class Graphs():
+    # TODO
     pass
 
 class BikeHornInterface():
@@ -112,11 +140,12 @@ class BikeHornInterface():
             self._gui.test_finished(success)
 
 class GUI():
-    def __init__(self, logging, bike_horn):
+    def __init__(self, logging, bike_horn:BikeHornInterface, results_manager:FilesManager):
         """Creates and runs the GUI
         """
         self._logging = logging
         self._bike_horn = bike_horn
+        self._results_manager = results_manager
         self._call_on_exit = []
 
 
@@ -142,6 +171,8 @@ class GUI():
         tab_control.add(timer2_settings_tab, text ='Timer 2 optimisation')
         upload_settings_tab = ttk.Frame(tab_control)
         tab_control.add(upload_settings_tab, text ='View / Upload Optimisations')
+        help_tab = ttk.Frame(tab_control)
+        tab_control.add(help_tab, text="Help / About")
         tab_control.pack(expand=True, fill ='both')
 
         # Message output
@@ -157,6 +188,7 @@ class GUI():
         # Fill up the tabs (after drawing the monitor so they can log to it)
         self._draw_run_test_tab(run_test_tab)
         self._draw_save_load_tab(save_load_tab)
+        self._draw_help_tab(help_tab)
 
         self._root.protocol("WM_DELETE_WINDOW", self._close_window)
     
@@ -164,7 +196,7 @@ class GUI():
         # Ready for interaction
         self._root.mainloop()
 
-    def confirm_run_test(self):
+    def _confirm_run_test(self):
         """Asks whether to run the test as any unsaved results and settings will be lost
         """
         result = tkmb.askyesno("Confirm run test", "Are you sure you want to run a NEW test? Any previous, unsaved results will be lost.")
@@ -179,20 +211,20 @@ class GUI():
             else:
                 self._bike_horn.set_serial_port(self.serial_port.get())
                 test_thread = threading.Thread(target=self._bike_horn.run_test, args=(piezo_range, boost_range, midi_range), daemon=True)
-                # TODO: Disable buttons
-                # TODO: Cancel button
                 test_thread.start()
                 self._test_control_button['text'] = "ABORT Test"
-                self._test_control_button['command'] = self.confirm_abort_test
+                self._test_control_button['command'] = self._confirm_abort_test
+                # TODO: Grey out settings
     
-    def confirm_abort_test(self):
+    def _confirm_abort_test(self):
         """Confirms the user is sure they want to abort the test"""
-        result = tkmb.askokcancel("Abort the test", "Are you sure you can to abort the test? Any results up until the current note will be saved in {}".format("TODO")) # TODO
-        self._bike_horn.abort_test()
+        result = tkmb.askokcancel("Abort the test", "Are you sure you can to abort the test? Any results up until the current note can be saved later")
+        if result:
+            self._bike_horn.abort_test()
     
     def test_finished(self, success):
         self._test_control_button['text'] = "Start Test"
-        self._test_control_button['command'] = self.confirm_run_test
+        self._test_control_button['command'] = self._confirm_run_test
 
         # Set the progress bar to 0 if note successful
         if not success:
@@ -215,7 +247,7 @@ class GUI():
         self._text_messages.configure(state='normal')
         self._text_messages.insert('end', "{}{}".format(msg,end))
         self._text_messages.configure(state='disabled')
-        self._text_messages.yview()
+        # TODO: Scroll to bottom self._text_messages.yview()
 
     def set_sound_level(self, level):
         try:
@@ -226,6 +258,11 @@ class GUI():
             raise sd.CallbackAbort()
 
     def add_to_call_on_exit(self, method):
+        """Adds a method that will be called when the gui window is closed
+
+        Args:
+            method (method): Method to call
+        """
         self._call_on_exit.append(method)
 
     def _draw_run_test_tab(self, tab):
@@ -239,14 +276,12 @@ class GUI():
         self._serial_port_dropdown.grid(column=1, row=0, padx=10, pady=10, sticky=tk.EW, columnspan=3)
 
         # Sound device
-        # TODO: This feels like it should be put elsewhere
         ttk.Label(tab, text="Audio level (preview):").grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
         self._sound_progress = ttk.Progressbar(tab, orient=tk.HORIZONTAL, mode='determinate')
         self._sound_progress.grid(column=1, row=1, columnspan=3, sticky=tk.NSEW, padx=10, pady=10)
 
         # Sweep settings
-        # TODO: Increments - duty cycles and midi notes?
-        # TODO: Checkbox to include 0?
+        # TODO: Move increments to inline with their respective field
         ttk.Label(tab, text="Piezo duty min (%):").grid(row=2, padx=10, pady=10, sticky=tk.W)
         self._piezo_duty_min_text = tk.StringVar(value=5)
         piezo_duty_min_spinbox = ttk.Spinbox(tab, from_=0, to=100, textvariable=self._piezo_duty_min_text)
@@ -288,14 +323,58 @@ class GUI():
         increments_frame.grid(row=5, column=0, columnspan=4, sticky=tk.NSEW)
 
         # Run test and progress bar
-        self._test_control_button = ttk.Button(tab, text="Start test", command=self.confirm_run_test)
+        self._test_control_button = ttk.Button(tab, text="Start test", command=self._confirm_run_test)
         self._test_control_button.grid(row=6, padx=10, pady=10, columnspan=4, sticky=tk.NSEW)
 
         self._test_progress = ttk.Progressbar(tab, orient=tk.HORIZONTAL, mode='determinate', length=800)
         self._test_progress.grid(column=0, row=7, columnspan=4, sticky=tk.NSEW, padx=10, pady=10)
 
     def _draw_save_load_tab(self, tab):
-        pass
+        ttk.Label(tab, text="Current file:").grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
+        self._current_file_text = tk.Text(tab, height=1, padx=5, pady=5)
+        self._current_file_text.grid(row=0, column=1, sticky=tk.NSEW, padx=10, pady=10)
+        self._set_current_file_text(self._results_manager.get_filename())
+        ttk.Button(tab, text="Open", command=self._select_open_file).grid(row=0, column=3, padx=10, pady=10, sticky=tk.E)
+        ttk.Button(tab, text="Save as", command=self._select_save_file).grid(row=0, column=4, padx=10, pady=10, sticky=tk.E)
+    
+    def _draw_help_tab(self, tab):
+        ttk.Label(tab, text="Bike Horn Optimiser version {}\nBy Jotham Gates\nThis is still a work in progress. For more info, go to:".format(version)).grid(row=0, column=0, padx=10, pady=(10, 0), sticky=tk.W)
+        # Link based off https://stackoverflow.com/a/23482749
+        github = "https://github.com/jgOhYeah/BikeHorn"
+        github_link = ttk.Label(tab, text=github, cursor="hand2", foreground="blue")
+        github_link.grid(padx=10, pady=(0, 10), sticky=tk.W, row=1, column=0)
+        github_link.bind("<Button-1>", lambda e: webbrowser.open_new_tab(github))
+        ttk.Label(tab, text="MAIN STEPS:\n1. Upload the optimising sketch to the horn.\n2. Run or open a test.\n3. Upload the optimised settings to the horn.\n4. Upload the main bike horn sketch to put the horn back in power saving mode.").grid(padx=10, pady=10, row=2, column=0, sticky=tk.W)
+
+    def _set_current_file_text(self, text):
+        self._current_file_text.configure(state='normal')
+        self._current_file_text.delete('1.0', 'end')
+        self._current_file_text.insert('end', text)
+        self._current_file_text.configure(state='disabled')
+
+    def _select_save_file(self):
+        self._results_manager.get_filename()
+        filename = fd.asksaveasfilename(defaultextension="npz", initialfile=self._results_manager.get_filename())
+        if type(filename) == str:
+            self._logging.info("Filename to use is: '{}'".format(filename))
+            self._set_current_file_text(filename)
+            self._results_manager.set_filename(filename)
+            self._results_manager.save_data()
+        else:
+            self._logging.info("Cancelled saving file")
+    
+    def _select_open_file(self):
+        result = tkmb.askyesno("Open an existing file", "Are you sure you want to open an existing file? Any unsaved data will be lost.")
+        if result:
+            filename = fd.askopenfilename(defaultextension="npz", initialfile=self._results_manager.get_filename())
+            if type(filename) == str:
+                self._results_manager.open(filename)
+                self._set_current_file_text(filename)
+            else:
+                self._logging.info("Cancelled opening file")
+        else:
+            self._logging.info("Cancelled opening file")
+
 
     def _close_window(self):
         """Calls everything this is meant to call before closing, then destroys the window
@@ -309,6 +388,8 @@ class GUI():
         self._root.destroy()
 
 class Logging():
+    """Class for logging events and displaying them in appropriate locations
+    """
     def __init__(self, gui:GUI=None) -> None:
         self._gui = gui
 
@@ -365,8 +446,9 @@ class AudioLevels():
 
 if __name__ == "__main__":
     logging = Logging()
+    results_manager = FilesManager(logging)
     bike_horn = BikeHornInterface(logging)
-    gui = GUI(logging, bike_horn)
+    gui = GUI(logging, bike_horn, results_manager)
     bike_horn.set_gui(gui)
     audio = AudioLevels(gui)
     logging.set_gui(gui)
